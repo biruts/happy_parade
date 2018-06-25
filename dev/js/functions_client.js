e166e6052486380ef694746b41272503ceb36983
leopoldo araujo <lpaa83@gmail.com>
2018-06-25 09:10:28 -0300

    arquivos do projeto

---
+++ dev/js/functions_client.js
@@ -0,0 +1,210 @@
+var APP = APP || {}
+APP.Happyparade = {
+    setUp: function(){
+        this.navBarFixed();
+        this.scrollBarNav();
+        this.tabPinteHappy();
+        this.validarFormDwonload();
+        this.habilitarDownload(0);
+        this.validarFormCadastro();
+        this.applyMask();
+        this.hashApply();
+    },
+    navBarFixed: function(){
+        $(window).on('scroll',function() {
+            var scrolltop = $(this).scrollTop();
+
+            if(scrolltop >= 390) {
+              $('#nav_fixed_client').fadeIn(110);
+            }
+
+            else if(scrolltop <= 390) {
+              $('#nav_fixed_client').fadeOut(110);
+            }
+          });
+    },
+    scrollBarNav: function(){
+        // jQuery for page scrolling feature - requires jQuery Easing plugin
+        $('.scrollnav').on('click', function(event) {
+            var $position = $($(this).attr('href')).offset().top;
+            $('html, body').stop().animate({
+                scrollTop: $position -77
+            }, 600);
+            event.preventDefault();
+        });
+    },
+    tabPinteHappy: function(){
+        var that = this;
+        $('.nav-pills a').click(function (e) {
+          e.preventDefault();
+          var etapa = $(this).attr("href");
+
+          that.removeMsg();
+
+          if(etapa == "#download"){
+            if(!$(".downloadHabilit").length){
+                $("#f_preCadastro").submit();
+            } else {
+                $(this).tab('show');
+            }
+          } else {
+            $(this).tab('show');
+          }
+        })
+    },
+    validarFormDwonload: function(){
+        var myLanguage = {
+            lengthTooShortStart : 'O valor de entrada é ',
+            lengthBadEnd: " caráter",
+            requiredFields: "O campo obrigatório",
+            badEmail: "Endereço de e-mail incorreto"
+        }
+        var that = this;
+        $.validate({
+            form : '#f_preCadastro',
+            validateOnBlur : true,
+            language : myLanguage,
+            onError: function () {
+                $(".btDownload").removeClass("downloadHabilit");
+            },
+            onSuccess : function($form) {
+                $('#carregando').modal('show');
+                $('#carregando').modal({backdrop: false});
+                that.ajaxPinteUmHappy($form);
+                $form.submit(function(){
+                    return false;
+                })
+                return false
+            }
+        });
+    },
+    ajaxPinteUmHappy: function($form){
+        var that = this;
+        var dados = $form.serialize();
+        $.ajax({
+            type: "POST",
+            url: "download-modelo.php",
+            data: dados,
+            beforeSend: function(){
+            },
+            success: function(data){
+                $('#carregando').modal('hide');
+                that.habilitarDownload(1);                
+                //$('#pinte .nav li:eq(1) a').tab('show'); 
+                $('#pinte .tab-pane#precadastro, .tab-pane#cadastro').removeClass('show active');
+                $('#pinte .tab-pane#download').addClass('active show');
+
+                $('.nav-pills li#dow a').addClass('active');
+                $('.nav-pills li#pre a, .nav-pills li#cad a').removeClass('active');
+
+                $('#msgSucess').modal('show');                
+            },
+            error: function(e) {
+                $('#carregando').modal('hide');
+                console.log(e);
+            }
+        });
+    },
+    habilitarDownload: function(habilita){
+        if(habilita){
+            $(".btDownload").addClass("downloadHabilit");
+        } else {
+            $(".btDownload").on("click", function(e){
+                if(!$(".btDownload").hasClass("downloadHabilit")){
+                    e.preventDefault();
+                }
+            });
+        }
+        
+    },
+    validarFormCadastro: function(){
+        var myLanguage = {
+            lengthTooShortStart : 'O valor de entrada é ',
+            lengthBadEnd: " caráter",
+            requiredFields: "Campo obrigatório",
+            badEmail: "Endereço de e-mail incorreto",
+            wrongFileSize: "O arquivo muito grande, tamanho maximo 16M",
+            wrongFileType: "Tipo de arquivo invalido"
+        }
+        var that = this;
+        $.validate({
+            form : '#contact',
+            modules : 'file',
+            validateOnBlur : true,
+            language : myLanguage,
+            onError: function () {
+            },
+            onSuccess : function($form) {
+                $('#carregando').modal('show');
+                $('#carregando').modal({backdrop: false});
+            }
+        });
+    },
+    msg: function($field, text){
+        var $msg = $('<span class="label label-danger msg-erro-form">'+text+'</span>');
+        $field.addClass("erro")
+        $field.append($msg);
+        $('html, body').stop().animate({
+            scrollTop: $field.offset().top - 65
+        }, 600);
+    },
+    removeMsg: function(){
+        $(document).find(".erro").removeClass("erro")
+        $(document).find(".msg-erro-form").remove();
+    },
+    applyMask: function(){
+        if($(".maskDate, .maskFone, .maskCelular").length){
+            $(".maskDate").mask("99/99/9999");
+            $(".maskFone").mask("99.99999-9999");
+            $(".maskCelular").mask("99.99999-9999");
+        }
+    },
+    hashApply: function(){
+        var hash = window.location.hash;
+        if(hash){
+            $('html, body').stop().animate({
+                scrollTop: $(hash).offset().top
+            }, 1000);
+        }
+    }
+};
+(function(){
+    APP.Happyparade.setUp();
+})();
+
+
+// $(document).on('click', '#preCad', function(e) {
+//     $('.nav-pills #cad').addClass('active');
+//     $('.nav-pills #pre, .nav-pills #dow').removeClass('active');    
+// });
+
+// $(document).on('click', '.nav-pills a', function(e) {
+//     $('#preCad').removeClass('active show');
+// });
+
+$(document).on('click', '#preCad', function(e) {
+    $('.nav-pills li#cad a').addClass('active');
+    $('.nav-pills li#pre a, .nav-pills li#dow a').removeClass('active');
+});
+
+$(document).on('click', '.nav-pills li a', function(e) {
+    $('.nav-pills li#preCad a').removeClass('active show');
+});
+
+
+//Carrega Galeria das vacas / Noticias
+$(document).ready(function() { 
+    $('#galeria-cowparade').load('galeria.html'); 
+    $('#noticias-cowparade').load('noticias.html'); 
+});
+//End Carrega Galeria das vacas / Noticias
+
+//Bloquear scroll no mapa
+$('.mapa').click(function () {
+    $('.mapa iframe').css("pointer-events", "auto");
+});
+
+$( ".mapa" ).mouseleave(function() {
+  $('.mapa iframe').css("pointer-events", "none"); 
+});
+//Bloquear scroll no mapa
---
+++ dist/download-modelo.php
@@ -0,0 +1,137 @@
+<?php
+
+    //$nome = $_POST['escolhaTemaNome'];
+    $nome_pre = utf8_decode($_POST["nome_pre"]);
+    $email_pre = utf8_decode($_POST['email_pre']);
+    $cidade_pre = utf8_decode($_POST['cidade_pre']);  
+    $tel_pre = utf8_decode($_POST['tel_pre']);
+    $datNasc_pre      = utf8_decode($_POST['datNasc_pre']);
+    $titulo_pre = utf8_decode($_POST['titulo_pre']);
+    $ip = utf8_decode($_SERVER['REMOTE_ADDR']);
+    $navegador = utf8_decode($_SERVER['HTTP_USER_AGENT']);
+
+
+
+    $headers = "MIME-Version: 1.0\r\n";
+    //$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+    //$headers = "MIME-Version: 1.1\n";
+    $headers .= "Content-Type: text/html;\r\n charset=\"UTF-8\"\r\n";
+    //$headers = "Content-Type: text/html; charset=UTF-8";
+    $headers .= "From: $nome_pre <$email_pre>\n"; // remetente
+    $headers .= "Return-Path: $nome_pre <$email_pre>\n"; // return-path
+    //$destinatario = 'producao.saopaulo@cowparade.com.br';
+    $destinatario = 'lpaa83@gmail.com';
+    $subject = 'Pré Cadastro | HappyArt Parade - SÃO PAULO';
+
+    $mensagem = '
+    <html>
+    <title>HappyArt Parade - SÃO PAULO</title>
+    <body>
+    <div style="width: 500px; margin: 10px auto;">
+    <table width="500px" cellspacing="0" cellpadding="0" border="0">
+        <thead>
+            <tr>
+                <th colspan="2"><img src="http://leoaraujo.com/yodesign/happy_parade/images/logo_client_2.png" alt="header"></th>
+            </tr>   
+            <tr>
+                <th colspan="2" style="font-size: 22px; color: #007cc3; font-family: arial; padding-bottom: 20px; text-align: center;">HappyArt Parade - SÃO PAULO</th>
+            </tr>
+        </thead>
+        <tbody style="font-family: arial; color: #1b2341; font-size: 15px;">
+            <tr bgcolor="#19cbff">
+                <td colspan="2" style="padding: 10px 5px; font-size: 20px; font-weight: bolder; text-align: center; color: #fff568;">Pré-Cadastro</td>
+            </tr>
+            <tr bgcolor="#fffbc6">
+                <td style="padding: 10px 5px; font-weight: bolder; width: 150px">Nome Completo:</td>
+                <td style="padding: 10px 5px; font-weight: normal; width: 350px; text-align: left;">'.$nome_pre.'</td>
+            </tr>
+            <tr bgcolor="#b3ecfd">
+                <td style="padding: 10px 5px; font-weight: bolder; width: 150px">E-mail:</td>
+                <td style="padding: 10px 5px; font-weight: normal; width: 350px; text-align: left;">'.$email_pre.'</td>
+            </tr>
+            <tr bgcolor="#fffbc6">
+                <td style="padding: 10px 5px; font-weight: bolder; width: 150px">Data de Nascimento:</td>
+                <td style="padding: 10px 5px; font-weight: normal; width: 350px; text-align: left;">'.$datNasc_pre.'</td>
+            </tr>
+            <tr bgcolor="#b3ecfd">
+                <td style="padding: 10px 5px; font-weight: bolder; width: 150px">Cidade:</td>
+                <td style="padding: 10px 5px; font-weight: normal; width: 350px; text-align: left;">'.$cidade_pre.'</td>                
+            </tr>
+            <tr bgcolor="#fffbc6">
+                <td style="padding: 10px 5px; font-weight: bolder; width: 150px">Telefone:</td>
+                <td style="padding: 10px 5px; font-weight: normal; width: 350px; text-align: left;">'.$tel_pre.'</td>
+            </tr>
+            <tr bgcolor="#b3ecfd">
+                <td style="padding: 10px 5px; font-weight: bolder; width: 150px">Título do Projeto:</td>
+                <td style="padding: 10px 5px; font-weight: normal; width: 350px; text-align: left;">'.$titulo_pre.'</td>
+            </tr>
+            <tr bgcolor="#ed1b2e">
+                <td colspan="2" style="padding: 1px; font-size: 10px; font-weight: bolder; text-align: left">&nbsp;</td>    
+            </tr>
+        </tbody>
+        <tfoot style="font-family: arial; color: #c0c1c1; font-size: 11px; font-weight: lighter;">
+            <tr>
+                <td colspan="2" align="center" style="padding: 20px;"><img src="http://leoaraujo.com/yodesign/happy_parade/images/footer_all.jpg" width="500px" height="auto" alt="footer"></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: center; padding-bottom: 30px;">
+                    Desenvolvido por: <a href="http://yodesign.com.br/" target="_blank" style="color:#c0c1c1; ">YO Design</a>
+                </td>
+            </tr>
+        </tfoot>    
+    </table>
+    </div> 
+
+    </body>                        
+    </html>
+    ';
+    $mensagemconfirmacao = '
+        <html>
+    <title>HappyArt Parade - SÃO PAULO</title>
+    <body>
+     <div style="width: 500px; margin: 10px auto;">
+    <table width="500px" cellspacing="0" cellpadding="0" border="0">
+        <thead>
+            <tr>
+                <th colspan="2"><img src="http://leoaraujo.com/yodesign/happy_parade/images/logo_client_2.png" alt="header"></th>
+            </tr>   
+            <tr>
+              <th colspan="2" style="font-size: 32px;text-align: center;">'.$nome_pre.'</th>
+            </tr>
+            <tr>
+                <th colspan="2" style="font-size: 22px; color: #007cc3; font-family: arial; padding-bottom: 20px; text-align: center;">Bem vindo ao HappyArt Parade</th>
+            </tr>
+        </thead>
+       
+        <tfoot style="font-family: arial; color: #c0c1c1; font-size: 11px; font-weight: lighter;">
+            <tr>
+                <td colspan="2" align="center" style="padding: 20px;"><img src="http://leoaraujo.com/yodesign/happy_parade/images/footer_all.jpg" width="500px" height="auto" alt="footer"></td>
+            </tr>
+            <tr>
+                <td colspan="2" style="text-align: center; padding-bottom: 30px;">
+                    Desenvolvido por: <a href="http://yodesign.com.br/" target="_blank" style="color:#c0c1c1; ">YO Design</a>
+                </td>
+            </tr>
+        </tfoot>    
+    </table>
+    </div>
+    </body>                        
+    </html>
+    ';
+
+    if(mail($destinatario,$subject,$mensagem,$headers)){
+    ?>
+    <script language="JavaScript">
+        $('#formulario').remove();
+    </script>
+    <?
+        echo '<div class="alert alert-success fade in text-sm-center"><h4>Mensagem enviada com sucesso!</h4></div>';
+    } else {
+        echo '<div class="alert alert-danger fade in text-sm-center"><h4>Erro ao enviar a mensagem</h4></div>';
+    }
+    if(mail($email_pre,$subject,$mensagemconfirmacao,$headers)){
+        echo '<div class="alert alert-success fade in text-sm-center"><h4>Funcionou</h4></div>';
+    } else {
+        echo '<div class="alert alert-success fade in text-sm-center"><h4>Nao pegou</h4></div>';
+    }
+?>
